8c4486a924fdcaa1d2b0efe8e49c42d04c265d01
ChillerDragon <ChillerDragon@gmail.com>
2024-02-07 18:11:43 +0800

    Add HotEditorCuiRects()

--- loader.h
+++ loader.h
@@ -3,7 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void (*plugin_ptr_t)(CUIRect &View1, CUIRect &View2, CUIRect &View3);
+typedef void (*cui_rects_ptr_t)(
+	CUIRect &View1,
+	CUIRect &View2,
+	CUIRect &View3,
+	CUIRect &View4,
+	CUIRect &View5,
+	CUIRect &View6);
 
 CUIRect placeholder1;
 CUIRect placeholder2;
@@ -29,8 +35,8 @@ void HotCuiRects(
 	}
 
 	dlerror();
-	plugin_ptr_t tick_ptr;
-	tick_ptr = (plugin_ptr_t)dlsym(handle, "hot_cui_rects");
+	cui_rects_ptr_t tick_ptr;
+	tick_ptr = (cui_rects_ptr_t)dlsym(handle, "hot_cui_rects");
 
 	error = dlerror();
 	if(error != NULL)
@@ -40,6 +46,48 @@ void HotCuiRects(
 	}
 
 	tick_ptr(View1, View2, View3, View4, View5, View6);
+
+	dlclose(handle);
+}
+
+typedef void (*editor_cui_rects_ptr_t)(
+	CEditor *pEditor,
+	CUIRect &View1,
+	CUIRect &View2,
+	CUIRect &View3,
+	CUIRect &View4,
+	CUIRect &View5,
+	CUIRect &View6);
+
+void HotEditorCuiRects(
+	CEditor *pEditor = nullptr,
+	CUIRect &View1 = placeholder1,
+	CUIRect &View2 = placeholder2,
+	CUIRect &View3 = placeholder3,
+	CUIRect &View4 = placeholder4,
+	CUIRect &View5 = placeholder5,
+	CUIRect &View6 = placeholder6)
+{
+	char *error;
+	void *handle = dlopen("../../ddnet_hotui/sample.so", RTLD_LAZY);
+	if(!handle)
+	{
+		fprintf(stderr, "%s\n", dlerror());
+		return;
+	}
+
+	dlerror();
+	editor_cui_rects_ptr_t tick_ptr;
+	tick_ptr = (editor_cui_rects_ptr_t)dlsym(handle, "editor_hot_cui_rects");
+
+	error = dlerror();
+	if(error != NULL)
+	{
+		fprintf(stderr, "%s\n", error);
+		return;
+	}
+
+	tick_ptr(pEditor, View1, View2, View3, View4, View5, View6);
 
 	dlclose(handle);
 }
--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cstring>
 
+#include "../ddnet/src/game/editor/editor.h"
 #include "../ddnet/src/game/client/ui_rect.h"
 #include "../ddnet/src/engine/graphics.h"
 
@@ -22,6 +23,22 @@ void hot_cui_rects(
 
 	Left.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
 	// Right.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
+}
+
+// View1, View2, ..
+// are set to the values you pass to EditorHotCuiRects()
+// if you do not pass values it will be set to an empty new CUIRect
+// pEditor might be a nullptr
+void editor_hot_cui_rects(
+	CEditor *pEditor,
+	CUIRect &View1,
+	CUIRect &View2,
+	CUIRect &View3,
+	CUIRect &View4,
+	CUIRect &View5,
+	CUIRect &View6)
+{
+	pEditor->MapView()->SetWorldOffset({32.0f * 10, 32.0f * 1});
 }
 
 }
