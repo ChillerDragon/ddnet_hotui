86d30b040b7ca50061b5d713a6a7f92c2233ed0b
ChillerDragon <ChillerDragon@gmail.com>
2024-02-07 18:01:30 +0800

    Allow passing up to 6 CUIRects

--- loader.h
+++ loader.h
@@ -3,9 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void (*plugin_ptr_t)(CUIRect &View);
+typedef void (*plugin_ptr_t)(CUIRect &View1, CUIRect &View2, CUIRect &View3);
 
-void HotUi(CUIRect &View)
+CUIRect placeholder1;
+CUIRect placeholder2;
+CUIRect placeholder3;
+CUIRect placeholder4;
+CUIRect placeholder5;
+CUIRect placeholder6;
+
+void HotUi(
+	CUIRect &View1 = placeholder1,
+	CUIRect &View2 = placeholder2,
+	CUIRect &View3 = placeholder3,
+	CUIRect &View4 = placeholder4,
+	CUIRect &View5 = placeholder5,
+	CUIRect &View6 = placeholder6)
 {
 	char *error;
 	void *handle = dlopen("../../ddnet_hotui/sample.so", RTLD_LAZY);
@@ -26,8 +39,7 @@ void HotUi(CUIRect &View)
 		return;
 	}
 
-	tick_ptr(View);
+	tick_ptr(View1, View2, View3, View4, View5, View6);
 
 	dlclose(handle);
 }
-
--- sample.cpp
+++ sample.cpp
@@ -6,12 +6,22 @@
 
 extern "C"{
 
-void ui_tick(CUIRect &View)
+// View1, View2, ..
+// are set to the values you pass to HotUi()
+// if you do not pass values it will be set to an empty new CUIRect
+void ui_tick(
+	CUIRect &View1,
+	CUIRect &View2,
+	CUIRect &View3,
+	CUIRect &View4,
+	CUIRect &View5,
+	CUIRect &View6)
 {
- 	CUIRect Left, Right;
-	View.VSplitMid(&Left, &Right, 5.0f);
+	CUIRect Left, Right;
+	View1.VSplitMid(&Left, &Right, 5.0f);
 
-	// Left.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
-	Right.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
+	Left.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
+	// Right.Draw(ColorRGBA(1, 1, 1, 0.25f), IGraphics::CORNER_ALL, 10.0f);
 }
+
 }
