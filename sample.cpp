e38d0b1fb2fca75e1289bd15b3d8da2bede54aeb
ChillerDragon <ChillerDragon@gmail.com>
2024-02-11 19:48:10 +0800

    Working fuzzy poc

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cstring>
 
+#include "../ddnet/src/base/system.h"
 #include "../ddnet/src/engine/keys.h"
 #include "../ddnet/src/game/editor/editor.h"
 #include "../ddnet/src/game/client/ui_rect.h"
@@ -43,6 +44,36 @@ void editor_hot_cui_rects(
 {
 }
 
+const char *str_find_nocase_char(const char *haystack, char needle)
+{
+	while(*haystack)
+	{
+		if(tolower((unsigned char)*haystack) == tolower(needle))
+			return haystack;
+		haystack++;
+	}
+
+	return 0;
+}
+
+bool chiller_fuzzy_match(const char *pHaystack, const char *pNeedle)
+{
+	if(!pNeedle || !pNeedle[0])
+		return false;
+	const char *pHit = str_find_nocase_char(pHaystack, pNeedle[0]);
+	printf("'== pHit=%s'\n", pHit);
+	for(int i = 0; i < str_length(pNeedle); i++)
+	{
+		if(!pHit)
+			return false;
+		char search = pNeedle[i];
+		pHit = str_find_nocase_char(pHit, search);
+		printf("'%c' hit=%s\n", search, pHit);
+	}
+	printf("'-------------------'\n");
+	return pHit;
+}
+
 // View1, View2, ..
 // are set to the values you pass to EditorHotCuiRects()
 // if you do not pass values it will be set to an empty new CUIRect
@@ -57,27 +88,88 @@ void editor_list_hot_cui_rects(
 	CUIRect &View5,
 	CUIRect &View6)
 {
-	auto UI = [pEditor]() -> CUI* { return pEditor->UI(); };
 
+	// printf("%s\n", str_find_nocase_char("add quad", 'a'));
+
+	auto UI = [pEditor]() -> CUI* { return pEditor->UI(); };
+	int m_Dialog = pEditor->m_Dialog;
+	int &m_PromptSelectedIndex = pEditor->m_PromptSelectedIndex;
+	std::vector<char *> &m_vpCompletePrompList = pEditor->m_vpCompletePrompList;
+	std::vector<const char *> &m_vpFilteredPrompList = pEditor->m_vpFilteredPrompList;
+	CLineInputBuffered<IO_MAX_PATH_LENGTH> &m_PromptInput = pEditor->m_PromptInput;
+
+	m_vpCompletePrompList.clear();
+	m_vpCompletePrompList.emplace_back((char *)"Add Quad");
+	m_vpCompletePrompList.emplace_back((char *)"Add Sound");
+	m_vpCompletePrompList.emplace_back((char *)"Add Group");
+	m_vpCompletePrompList.emplace_back((char *)"Add tele layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add speedup layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add tune layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add front layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add switch layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add quads layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add tile layer");
+	m_vpCompletePrompList.emplace_back((char *)"Add sound layer");
+	m_vpCompletePrompList.emplace_back((char *)"Save As");
+	m_vpCompletePrompList.emplace_back((char *)"Save Copy");
+	m_vpCompletePrompList.emplace_back((char *)"Toggle High Detail");
+	m_vpCompletePrompList.emplace_back((char *)"Refocus");
+	m_vpCompletePrompList.emplace_back((char *)"Envelopes");
+	m_vpCompletePrompList.emplace_back((char *)"Server settings");
+	m_vpCompletePrompList.emplace_back((char *)"History");
+	m_vpCompletePrompList.emplace_back((char *)"Toggle preview of how layers will be zoomed in-game");
+
+	CUIRect Suggestions;
 
 	View.HSplitMid(&Prompt, nullptr);
 
 	Prompt.VSplitMid(nullptr, &PromptBox);
 
 	Prompt.Draw(ColorRGBA(0, 0, 0, 0.75f), IGraphics::CORNER_ALL, 10.0f);
-	Prompt.HSplitTop(32.0f, &PromptBox, nullptr);
+	Prompt.HSplitTop(32.0f, &PromptBox, &Suggestions);
 	PromptBox.Draw(ColorRGBA(1, 0, 0, 0.75f), IGraphics::CORNER_ALL, 10.0f);
 
-	UI()->DoLabel(&Prompt, "Search:", 10.0f, TEXTALIGN_ML);
-
-	if(UI()->DoClearableEditBox(&pEditor->m_PromptInput, &PromptBox, 10.0f))
+	if(UI()->DoClearableEditBox(&m_PromptInput, &PromptBox, 10.0f))
 	{
+		m_PromptSelectedIndex = 0;
+		m_vpFilteredPrompList.clear();
+		for(const char *Item : m_vpCompletePrompList)
+		{
+			// if(m_PromptInput.IsEmpty() || str_find_nocase(Item, m_PromptInput.GetString()))
+			if(m_PromptInput.IsEmpty() || chiller_fuzzy_match(Item, m_PromptInput.GetString()))
+			{
+				m_vpFilteredPrompList.push_back(Item);
+			}
+		}
+	}
+
+	s_ListBox.SetActive(m_Dialog == DIALOG_NONE && !UI()->IsPopupOpen());
+	s_ListBox.DoStart(15.0f, m_vpFilteredPrompList.size(), 1, 5, m_PromptSelectedIndex, &Suggestions, false);
+
+	for(size_t i = 0; i < m_vpFilteredPrompList.size(); i++)
+	{
+		const CListboxItem Item = s_ListBox.DoNextItem(m_vpFilteredPrompList[i], m_PromptSelectedIndex >= 0 && (size_t)m_PromptSelectedIndex == i);
+		if(!Item.m_Visible)
+			continue;
+
+		CUIRect Button, FileIcon, TimeModified;
+		Item.m_Rect.VSplitLeft(Item.m_Rect.h, &FileIcon, &Button);
+		Button.VSplitLeft(5.0f, nullptr, &Button);
+		Button.VSplitRight(100.0f, &Button, &TimeModified);
+		Button.VSplitRight(5.0f, &Button, nullptr);
+
+		SLabelProperties Props;
+		Props.m_MaxWidth = Button.w;
+		Props.m_EllipsisAtEnd = true;
+		UI()->DoLabel(&Button, m_vpFilteredPrompList[i], 10.0f, TEXTALIGN_ML, Props);
 
 	}
 
-	int m_FilesSelectedIndex = 0;
-
-	s_ListBox.DoStart(15.0f, pEditor->m_vpFilteredPrompList.size(), 1, 5, m_FilesSelectedIndex, &View, false);
+	const int NewSelected = s_ListBox.DoEnd();
+	if(m_PromptSelectedIndex != NewSelected)
+	{
+		m_PromptSelectedIndex = NewSelected;
+	}
 
 }
 
