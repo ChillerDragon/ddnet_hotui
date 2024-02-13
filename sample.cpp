2535c85b19c55cd4cf05fa0fcd180e8998f08baa
ChillerDragon <ChillerDragon@gmail.com>
2024-02-13 15:38:58 +0800

    This seems to be a solved problem

--- sample.cpp
+++ sample.cpp
@@ -41,7 +41,37 @@ void editor_hot_cui_rects(
 {
 	#include "scopes/editor_scope.h"
 
-	MapView()->SetWorldOffset({32.0f * 10, 32.0f * 1});
+	std::vector<CLayerSelector::CHoverTile> &m_vHoverTiles = pEditor->LayerSelector()->m_vHoverTiles;
+
+	for(auto HoverTile : m_vHoverTiles)
+	{
+		const std::shared_ptr<CLayerGroup> pGroup = m_Map.m_vpGroups[HoverTile.m_Group];
+		const std::shared_ptr<CLayer> pLayer = pGroup->m_vpLayers[HoverTile.m_Layer];
+
+		float aPoints[4];
+		pGroup->Mapping(aPoints);
+		float TopLeftX = aPoints[0];
+		float TopLeftY = aPoints[1];
+		float BottomRightX = aPoints[2];
+		float BottomRightY = aPoints[3];
+
+		float WorldWidth = aPoints[2] - aPoints[0];
+		float WorldHeight = aPoints[3] - aPoints[1];
+
+		// int x = aPoints[0] + WorldWidth * ((HoverTile.m_X * 32) / Graphics()->WindowWidth());
+		// int y = aPoints[1] + WorldHeight * ((HoverTile.m_Y * 32) / Graphics()->WindowHeight());
+		// m_MouseDeltaWx = m_MouseDeltaX * (WorldWidth / Graphics()->WindowWidth());
+		// m_MouseDeltaWy = m_MouseDeltaY * (WorldHeight / Graphics()->WindowHeight());
+
+		float TileScale = 64 / pEditor->m_MouseWScale;
+		int x = HoverTile.m_X * TileScale - MapView()->GetWorldOffset().x;
+		int y = HoverTile.m_Y * TileScale - MapView()->GetWorldOffset().y;
+
+		dbg_msg("c", "tile at %d %d", x, y);
+		Graphics()->TextureClear();
+		Graphics()->SetColor(vec4(0.0f, 1.0f, 0.0f, 1.0f));
+		Graphics()->DrawRect(x, y, TileScale, TileScale, ColorRGBA(0.0f, 0.0f, 1.0f, 0.5f), IGraphics::CORNER_ALL, 3.0f);
+	}
 }
 
 }
