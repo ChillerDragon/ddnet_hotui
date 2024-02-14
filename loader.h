161bcf8d5e91ac924293f844d74e840bf86979fc
ChillerDragon <ChillerDragon@gmail.com>
2024-02-14 11:29:10 +0800

    non working hotreload

--- loader.h
+++ loader.h
@@ -2,10 +2,143 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <error.h>
+#include <limits.h>
+#include <poll.h>
+#include <time.h>
+#include <sys/inotify.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define HOT_SO_PATH "../../ddnet_hotui/sample.so"
+
+#define READ_ONLY_OPERATION	(IN_ACCESS | IN_OPEN | IN_CLOSE_NOWRITE)
+
+int last_so_update_time = -1;
+int inotify_fd = -1;
+int inotify_watch = -1;
+
+int add_watch()
+{
+	// allow calling it multiple times without breaking it
+	if(inotify_fd == -1)
+		inotify_fd = inotify_init ();
+	if (inotify_fd == -1)
+	{
+		dbg_msg("ddnet_hotui", "Error: failed to init inotify fd");
+		exit(1);
+	}
+
+	// inotify can only watch files that exist
+	// so while it is delted we just wait
+	if (access(HOT_SO_PATH, F_OK) != 0)
+	{
+		dbg_msg("ddnet_hotui", "waiting for %s to appear ...", HOT_SO_PATH);
+		// we already listen for deletion of the file
+		// but seems like that is not catching every swap `make` triggers
+		inotify_watch = -1;
+		return 0;
+	}
+
+	// struct stat attr;
+	// if(stat(HOT_SO_PATH, &attr))
+	// {
+	// 	dbg_msg("ddnet_hotui", "failed to get stats for %s", HOT_SO_PATH);
+	// 	return;
+	// }
+	// int time = attr.st_mtime;
+	// if(time > last_so_update_time)
+	// {
+	// 	// force reload if the date updated
+	// 	// we already check with inotify but it does not seem to work
+	// 	// every time
+	// 	inotify_watch = -1;
+	// 	last_so_update_time = time;
+	// }
+
+	if(inotify_watch != -1)
+		return 0;
+
+	// when i compiled the so file and listening for all events
+	// i got back in this order
+	// IN_ATTRIB (4), IN_DELETE_SELF (1024), IN_IGNORED (32768)
+	// so lets only listen for the last one
+	inotify_watch = inotify_add_watch (inotify_fd, HOT_SO_PATH, IN_IGNORED);
+	if (inotify_watch == -1)
+	{
+		dbg_msg("ddnet_hotui", "Error: failed watch using inotify");
+		exit(1);
+	}
+
+	printf("Watching %s using inotify_watch %d\n", HOT_SO_PATH, inotify_watch);
+	return 1;
+}
+
+int got_new_so()
+{
+	if(add_watch())
+		return 1;
+	if(inotify_fd == -1)
+		return 1;
+	struct pollfd poll_fd;
+	poll_fd.fd = inotify_fd;
+
+	poll_fd.events = POLLIN;
+	poll_fd.revents = 0;
+	int a = poll(&poll_fd, 1, 0);
+	dbg_msg("ddnet_hotui", "polling ...");
+	if(a == 0)
+		return 0;
+	if(a < 0)
+	{
+		dbg_msg("ddnet_hotui", "poll returned %d", a);
+		return 0;
+	}
+	// TODO: we do not really care what we read here
+	//       but we have to consume the event
+	//       there has to be a way to do so without reading
+	char aBuf[10 * (sizeof(struct inotify_event))];
+	int num_read = read(poll_fd.fd, aBuf, sizeof(aBuf));
+	dbg_msg("ddnet_hotui", "num_read=%d", num_read);
+	if(num_read < 1)
+	{
+		dbg_msg("ddnet_hotui", "bad num_read=%d", num_read);
+		exit(1);
+	}
+	int got_update = 0;
+	for (char *p = aBuf; p < aBuf + num_read; ) {
+		struct inotify_event *pEvent = (struct inotify_event *) p;
+		dbg_msg("ddnet_hotui", "got event with mask %d", pEvent->mask);
+		// file deleted
+		if(pEvent->mask & IN_IGNORED)
+		{
+			// cleanup watch of dead inode
+			inotify_rm_watch(inotify_fd, inotify_watch);
+			// triggers reconnect if it appears again
+			inotify_watch = -1;
+			dbg_msg("ddnet_hotui", "file deleted %s", HOT_SO_PATH);
+		}
+		else if(pEvent->mask & READ_ONLY_OPERATION)
+		{
+			// we call stat() on the file our self
+			// this is not considered an update
+		}
+		else
+		{
+			dbg_msg("ddnet_hotui", "reloading because of inotify mask %d", pEvent->mask);
+			got_update = 1;
+		}
+
+		p += sizeof(struct inotify_event) + pEvent->len;
+	}
+
+	return got_update;
+}
+
 
 class CEditor;
-
-int hot_reload_tick = 0;
 
 typedef void (*cui_rects_ptr_t)(
 	CUIRect &View1,
@@ -31,7 +164,7 @@ void HotCuiRects(
 	CUIRect &View6 = placeholder6)
 {
 	char *error;
-	void *handle = dlopen("../../ddnet_hotui/sample.so", RTLD_LAZY);
+	void *handle = dlopen(HOT_SO_PATH, RTLD_LAZY);
 	if(!handle)
 	{
 		fprintf(stderr, "%s\n", dlerror());
@@ -74,14 +207,16 @@ void HotEditorCuiRects(
 	CUIRect &View5 = placeholder5,
 	CUIRect &View6 = placeholder6)
 {
-	hot_reload_tick++;
-	if(hot_reload_tick > 500)
+	if(got_new_so())
 	{
-		hot_reload_tick = 0;
+		dbg_msg("ddnet_hotui", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		dbg_msg("ddnet_hotui", "X ddnet_hotui - reloading ... X");
+		printf("X inotify: %s            X\n", inotify_fd == -1 ? "ERROR  " : "RUNNING");
+		dbg_msg("ddnet_hotui", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 		char *error;
 		if (handle)
 			dlclose(handle);
-		handle = dlopen("../../ddnet_hotui/sample.so", RTLD_LAZY);
+		handle = dlopen(HOT_SO_PATH, RTLD_LAZY);
 		if(!handle)
 		{
 			fprintf(stderr, "%s\n", dlerror());
