b4daa84411a2d6e932a7fedc6b3f18da4bab8d1f
ChillerDragon <ChillerDragon@gmail.com>
2024-03-21 08:49:07 +0800

    Support new ddnet camel casing

--- scopes/editor_scope.h
+++ scopes/editor_scope.h
@@ -1,3 +1,12 @@
+// back compat for
+// https://github.com/ddnet/ddnet/pull/8019
+#ifndef CUI
+	#define CUI CUi
+#endif
+#ifndef Ui
+	#define UI Ui
+#endif
+
 auto UI = [pEditor]() -> CUI* { return pEditor->UI(); };
 auto MapView = [pEditor]() -> CMapView * { return pEditor->MapView(); };
 auto Graphics = [pEditor]() -> IGraphics * { return pEditor->Graphics(); };
